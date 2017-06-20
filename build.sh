96176c5b225d15af0b2a73f3fab395fdf6a7a9b9
pouya <pouya@applariat.com>
2017-06-20 11:45:50 -0700

    enabled build logs, fixed copy and npm cache clean commands

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #! /bin/sh
 # appLariats generic tomcat build script
-# Requirements - A valid war file exists within the code_dir
-# Copies the war file from the code_dir and copies it into /usr/local/tomcat/webapps
+# Requirements - A valid war file exists within the src_dir
+# Copies the war file from the src_dir and copies it into /usr/local/tomcat/webapps
+
+#Log everything in /src/build.log
+logfile=/src/build.log
+exec > $logfile 2>&1
+set -x
 
 mkdir -p /usr/src/app
 
 #Check for package.json and throw exception if not present
-if [ -e $artifact_root/package.json ]
+if [ -e /src/package.json ]
 then
-    cp -f $artifact_root/package.json /usr/src/app/
+    cp -rf /src/package.json /usr/src/app/
 else
     echo "ERROR! package.json not found"
     exit 1
 fi
 
-npm install && npm cache clean
+npm install && npm cache clean --force
+
+cp -rf /src/* /usr/src/app
